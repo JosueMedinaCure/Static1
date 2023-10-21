3c32fb5acd587593adaa924bd857f7e21323ed71
josue <jmcure2@gmail.com>
2023-10-21 00:18:04 -0500

    adition toggle red and white colors

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+imagenGrande = document.querySelector(".papa");
+
+imagenGrande.addEventListener("click",function(){
+
+        this.classList.toggle('papa');
+        this.classList.toggle('imagen_1');
+});
